cceb0c6fb9ebe62ca26ce0c20e1ba2fe9c4a2d7e
Alfredo <21030761@itcelaya.edu.mx>
2024-01-15 09:01:11 -0600

    SEED started

---
+++ postgresql/seed/data/user_blogger.seed.js
@@ -0,0 +1,11 @@
+// import { Product } from "src/product/interfaces/product.interface";
+// import { v4 as uuidv4 } from 'uuid';
+const table = 'USER_BLOGGER';
+
+export const USERS_BLOGGER_SEED = [
+    {id: uuidv4(), product: 'Chancla', price: 20, category: 'clothes'},
+    {id: uuidv4(), product: 'Chamarrin', price: 500, category: 'clothes'},
+    {id: uuidv4(), product: 'Tenis', price: 1000, category: 'clothes'},
+    {id: uuidv4(), product: 'Gorro', price: 200, category: 'clothes'},
+    {id: uuidv4(), product: 'Celular', price: 200, category: 'electronics'},
+];
---
+++ postgresql/seed/index.js
@@ -0,0 +1,1 @@
+const Ctrl = require('./controller');
